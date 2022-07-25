10cce8c3b2541ab90294fb51e6b48a99ee798166
Tolga <06thy40@gmail.com>
2022-07-26 01:29:22 +0300

    added navbar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 
 const NavBar = () => {
   return (
-    <Navbar bg="dark">
+    <Navbar bg="dark" className="p-3">
       <Container>
         <h3 className="text-light">Countries App</h3>
       </Container>
